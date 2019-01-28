51686eb1bfae3328897f5585c1ecd198df9a5851
Bozotte <armelle.peuvion.weiss@gmail.com>
2019-01-28 14:06:06 +0100

    ajouts6

--- config.ru
+++ config.ru
@@ -2,6 +2,6 @@ require 'bundler'  # appellent le bundler, pas besoin de mettre des require 'gem
 Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
-require './controller'
+require 'controller'
 
 run ApplicationController
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,16 @@
+require 'bundler'
+Bundler.require
+
 class ApplicationController < Sinatra::Base ##classe ApplicationController hérite de la classe Sinatra::Base = ApplicationController aura toutes les fonctionnalités de la classe Sinatra::Base.
   get '/' do # "si quelqu'un va sur l'adresse '/', exécute ce code !"
     erb :index
-    erb :new_gossip
     #"<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+  end
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post 'gossips/new/' do
+    puts "Hello World"
   end
 end
