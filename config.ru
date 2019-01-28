2c0d7dc3c9e4b5a56926ea84649558ca41d94ff1
Bozotte <armelle.peuvion.weiss@gmail.com>
2019-01-28 12:22:13 +0100

    ajouts3

--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
-require 'bundler'
+require 'bundler'  # appellent le bundler, pas besoin de mettre des require 'gem'.
 Bundler.require
 
+$:.unshift File.expand_path("./../lib", __FILE__)
 require './controller'
 
 run ApplicationController
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,10 +1,7 @@
-require 'bundler' # appellent le bundler, pas besoin de mettre des require 'gem'.
-Bundler.require
-
 class ApplicationController < Sinatra::Base ##classe ApplicationController hérite de la classe Sinatra::Base = ApplicationController aura toutes les fonctionnalités de la classe Sinatra::Base.
   get '/' do # "si quelqu'un va sur l'adresse '/', exécute ce code !"
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index
+    erb :new_gossip
+    #"<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
   end
-
-  run! if app_file == $0 # permet d'éxecuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier. Il faut forcément l'exécution d'une méthode en bas de la classe pour lancer la machine ! C'est ce que fait cette ligne.
 end
